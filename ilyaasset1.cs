a47700284c122144c4c797aea11b15042c125a2b
ilka8900 <85946037+ilka8900@users.noreply.github.com>
2023-12-03 16:56:39 +0300

    Add files via upload

---
+++ ilyaasset1.cs
@@ -0,0 +1,27 @@
+using OpenCover.Framework.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ilyaasset1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello world");
+        int a,b,c,s;
+        a = 7;
+        b = 8;
+        c = 11;
+        s = a + b + c;
+        print(s);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
